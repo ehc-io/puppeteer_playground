3415d1b3ec14fd98c566febebc1dbb201022d050
ehc-io <eduardo@ehc.io>
2020-07-25 08:54:47 -0300

    improving scraping throught page object options (forEach)

--- myscrape.js
+++ myscrape.js
@@ -4,15 +4,27 @@ const targetUrl = 'http://books.toscrape.com/';
 const cssBooksForPage = 'li.col-xs-6';
 
 const scrape = async () => {
+  console.log('Running...');
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(targetUrl);
   await page.waitFor(2 * 1000);
-  console.log('--------');
 
-  const result = await page.$$eval(cssBooksForPage, elArray =>
-    elArray.map(el => el.innerText)
-  );
+  // const result = await page.evaluate(sel => {
+  //   const bookArray = [];
+  //   const elArray = document.querySelectorAll(sel);
+  //   // const bookArray = elArray.forEach(el => el.innerText);
+  //   for (let i = 0; i < elArray.length; i += 1) {
+  //     bookArray.push(elArray[i].innerText);
+  //   }
+  //   return bookArray;
+  // }, cssBooksForPage);
+
+  const result = await page.evaluate(sel => {
+    const elArray = document.querySelectorAll(sel);
+    const bookArray = elArray.forEach(el => el.innerText);
+    return bookArray;
+  }, cssBooksForPage);
 
   // const result = await page.$$eval(cssBooksForPage, elArray =>
   //   elArray.map(el => el.innerText)
