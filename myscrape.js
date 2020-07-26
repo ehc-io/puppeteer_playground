04a87e01110b9d948ee61df4490ca2ac771706e1
ehc-io <eduardo@ehc.io>
2020-07-26 08:26:25 -0300

    select book & click ->Page.evaluate/evaluateHandle Page

--- myscrape.js
+++ myscrape.js
@@ -2,34 +2,50 @@ const puppeteer = require('puppeteer');
 
 const targetUrl = 'http://books.toscrape.com/';
 const cssBooksForPage = 'li.col-xs-6';
+const cssBookLink =
+  '#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a';
 
 const scrape = async () => {
   console.log('Running...');
-  const browser = await puppeteer.launch({ headless: true });
+  console.log('Initiating browser session...');
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreHTTPSErrors: true,
+    // devtools: true,
+    // userDataDir: './chromium-profile',
+    args: [
+      "--proxy-server='direct://",
+      '--disable-extensions',
+      '--proxy-bypass-list=*',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
   const page = await browser.newPage();
   await page.goto(targetUrl);
   await page.waitFor(2 * 1000);
 
-  // const result = await page.evaluate(sel => {
-  //   const bookArray = [];
-  //   const elArray = document.querySelectorAll(sel);
-  //   // const bookArray = elArray.forEach(el => el.innerText);
-  //   for (let i = 0; i < elArray.length; i += 1) {
-  //     bookArray.push(elArray[i].innerText);
-  //   }
-  //   return bookArray;
-  // }, cssBooksForPage);
+  const result = await page.$$eval(cssBooksForPage, elArray =>
+    elArray.map(el => el.innerText)
+  );
 
-  const result = await page.evaluate(sel => {
-    const elArray = document.querySelectorAll(sel);
-    const bookArray = elArray.forEach(el => el.innerText);
-    return bookArray;
-  }, cssBooksForPage);
+  // const bookHandle = await page.evaluateHandle(function(sel) {
+  //   return document.querySelector(sel);
+  // }, cssBookLink);
 
-  // const result = await page.$$eval(cssBooksForPage, elArray =>
-  //   elArray.map(el => el.innerText)
-  // );
+  const bookHandle = await page.evaluateHandle(
+    sel => document.querySelector(sel),
+    cssBookLink
+  );
 
+  // await page.evaluateHandle(function(link) {
+  //   link.click();
+  // }, bookHandle);
+
+  await page.evaluateHandle(link => link.click(), bookHandle);
+
+  await page.waitFor(1 * 1000);
+  await page.screenshot({ path: 'screenshots/book.png' });
   browser.close();
   return result;
 };
