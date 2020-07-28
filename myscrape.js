5aa0ba35e4cd44523dc2d1c0c205b2285c9bcbf9
ehc-io <eduardo@ehc.io>
2020-07-27 21:33:21 -0300

    added pagination

--- myscrape.js
+++ myscrape.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const targetUrl = 'http://books.toscrape.com/';
 const cssBooksForPage = 'li.col-xs-6';
 
-const scrape = async () => {
+async function initbrowser() {
   console.log('Running...');
   console.log('Initiating browser session...');
   const browser = await puppeteer.launch({
@@ -19,37 +19,57 @@ const scrape = async () => {
       '--disable-setuid-sandbox',
     ],
   });
+  return browser;
+}
+
+async function scrape(browser, pagesToScrape) {
+  if (!pagesToScrape) {
+    pagesToScrape = 1;
+  }
+  console.log(`Pages to scrape: ${pagesToScrape}`);
   const page = await browser.newPage();
   await page.goto(targetUrl);
   await page.waitFor(1 * 1000);
-
-  const booksForPage = await page.evaluate(function(sel) {
-    const elArray = [];
-    const items = document.querySelectorAll(sel);
-    items.forEach(function(book) {
-      elArray.push({
-        url: book.children[0].children[0].children[0].href,
-        title: book.children[0].children[2].innerText,
-      });
-    });
-    // for (let i = 0; i < items.length; i += 1) {
-    //   elArray.push({
-    //     url: items[i].children[0].children[0].children[0].href,
-    //     title: items[i].children[0].children[2].innerText,
-    //   });
-    // }
-    return elArray;
-  }, cssBooksForPage);
+  let currentPage = 1;
+  let bookStore = [];
   //
-  booksForPage.forEach(function(book) {
-    console.log(`${book.title}`);
-  });
-  console.log(`There are ${booksForPage.length} elements in this page`);
-  await page.waitFor(100 * 1000);
-  await page.screenshot({ path: 'screenshots/book.png' });
+  while (currentPage <= pagesToScrape) {
+    console.log(`Scraping page: ${currentPage}`);
+    const booksForPage = await page.evaluate(function(sel) {
+      const elArray = [];
+      const items = document.querySelectorAll(sel);
+      items.forEach(function(book) {
+        elArray.push({
+          url: book.children[0].children[0].children[0].href,
+          title: book.children[0].children[2].innerText,
+        });
+      });
+      return elArray;
+    }, cssBooksForPage);
+    booksForPage.forEach(function(book) {
+      console.log(`${book.title}`);
+    });
+    console.log(`----- end of page ----`);
+    bookStore = bookStore.concat(booksForPage);
+    await page.screenshot({ path: 'screenshots/book.png' });
+    // await page.waitFor(3 * 1000);
+    if (currentPage < pagesToScrape) {
+      await page.click('li.next');
+      await page.waitFor(1 * 1000);
+    }
+    currentPage += 1;
+  }
+  console.log(`There are ${bookStore.length} books in this book store`);
   browser.close();
-};
+  return bookStore;
+  //
+}
 
-scrape().then(value => {
-  console.log(value); // Success!
-});
+async function main() {
+  const browserSession = await initbrowser();
+  scrape(browserSession, 5)
+    .then(() => console.log(`That was a huge success!!!`))
+    .catch(e => console.log(`Sorry, my bad !!! I've got an error: ${e}`));
+}
+
+main();
